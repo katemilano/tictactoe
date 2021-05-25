cce57fb8e7fa5607cda2c0efbc0967db05520b7c
Kate Milano <catherine.ann.milano@gmail.com>
2021-05-25 14:57:21 -0400

    game inital code

--- script.js
+++ script.js
@@ -3,18 +3,25 @@ $(window).on('load', function() {
     var ptags = $("p");
     ptags.each(function(i, element){
         $(element).attr("style","opacity:0;")
-        console.log($(element));
-
     });
     
 })
 
 //When you click on the box game starts
+var count = 0;
 
-// function showMove(event){
-//     console.log(event);
-//     console.log(event.target);
-// }
+function showMove(){
+    var p = $(this).children('p')[0];
+
+    if (count % 2 === 0){
+        let p = $(this).children('p')[0];
+        $(p).attr("style", "opacity:1;")
+    }else{
+        let p = $(this).children('p')[1];
+        $(p).attr("style", "opacity:1;")
+    }
+   count++;
+}
 
 
-// box.addEventListener("click", showMove(event));
+$(".cell").on("click", showMove);
